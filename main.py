d707d27a550ee4233cbd27346e07a547e2528148
GovardFilipsovich <ricardrajonhart@gmail.com>
2021-04-12 19:44:43 +0300

    Добавлены страницы регистрации и входа, без реализации соответствующего функционала, а также переход на них(ерез отдельные ссылки)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'AllanGregoryPrimaryKey'
@@ -9,14 +8,24 @@ app.config['SECRET_KEY'] = 'AllanGregoryPrimaryKey'
 @app.route('/index', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        print(1)
-        print(request.form)
         return render_template("index.html")
     else:
         if list(request.form.keys())[0] == "SignUp":
-            return render_template("registration.html")
+            return redirect("/reg")
         elif list(request.form.keys())[0] == "SignIn":
-            return render_template("signin.html")
+            return redirect("/signin")
+
+
+@app.route('/reg', methods=["GET", "POST"])
+def reg():
+    if request.method == "GET":
+        return render_template("registration.html")
+
+
+@app.route('/signin', methods=["GET", "POST"])
+def signin():
+    if request.method == "GET":
+        return render_template("signin.html")
 
 
 if __name__ == '__main__':
