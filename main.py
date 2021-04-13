aec4090a8df5c7d6ce4d67d570b46e20815cfc3a
GovardFilipsovich <ricardrajonhart@gmail.com>
2021-04-13 22:02:26 +0300

    Добавлена кнопка меню(На главное меню), а также объект меню

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ app.config['SECRET_KEY'] = 'AllanGregoryPrimaryKey'
 @app.route('/index', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("index.html")
+        return render_template("index.html", start_menu="-25%")
     else:
         if list(request.form.keys())[0] == "SignUp":
             return redirect("/reg")
         elif list(request.form.keys())[0] == "SignIn":
             return redirect("/signin")
+        elif list(request.form.keys())[0] == "Sett":
+            return render_template("index.html", start_menu="0%")
 
 
 @app.route('/reg', methods=["GET", "POST"])
@@ -26,6 +28,11 @@ def reg():
 def signin():
     if request.method == "GET":
         return render_template("signin.html")
+
+
+@app.route("/<Username>/profile")
+def profile(Username):
+    return render_template("profile.html")
 
 
 if __name__ == '__main__':
