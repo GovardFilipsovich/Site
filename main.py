864732d518540030ff277aae5aabeedbc4da4882
GovardFilipsovich <ricardrajonhart@gmail.com>
2021-04-14 07:24:23 +0300

    Усовершенствован объект меню. Сделана логика открытия меню

--- main.py
+++ main.py
@@ -3,19 +3,28 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'AllanGregoryPrimaryKey'
 
+count_menu = 0
 
 @app.route('/', methods=["GET", "POST"])
 @app.route('/index', methods=["GET", "POST"])
 def index():
+    global count_menu
     if request.method == "GET":
-        return render_template("index.html", start_menu="-25%")
+        count_menu = 0
+        return render_template("index.html", hidden="hidden")
     else:
         if list(request.form.keys())[0] == "SignUp":
             return redirect("/reg")
         elif list(request.form.keys())[0] == "SignIn":
             return redirect("/signin")
         elif list(request.form.keys())[0] == "Sett":
-            return render_template("index.html", start_menu="0%")
+            print(count_menu)
+            if count_menu % 2 == 0:
+                count_menu += 1
+                return render_template("index.html", hidden="")
+            else:
+                count_menu += 1
+                return render_template("index.html", hidden="hidden")
 
 
 @app.route('/reg', methods=["GET", "POST"])
