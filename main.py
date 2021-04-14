e4c2d430f92e2cfde7797a183d3ff1910da1d2e2
GovardFilipsovich <ricardrajonhart@gmail.com>
2021-04-14 19:50:30 +0300

    Создана основа для страницы пользователя. Создана форма авторизации. Изменена структура ссылок

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template, request, redirect
+from loginform import LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'AllanGregoryPrimaryKey'
 
 count_menu = 0
 
-@app.route('/', methods=["GET", "POST"])
-@app.route('/index', methods=["GET", "POST"])
+@app.route("/")
+@app.route("/index")
 def index():
+    return redirect("/guest/profile")
+
+
+@app.route("/guest/profile", methods=["GET", "POST"])
+def guest():
     global count_menu
     if request.method == "GET":
         count_menu = 0
@@ -18,7 +24,6 @@ def index():
         elif list(request.form.keys())[0] == "SignIn":
             return redirect("/signin")
         elif list(request.form.keys())[0] == "Sett":
-            print(count_menu)
             if count_menu % 2 == 0:
                 count_menu += 1
                 return render_template("index.html", hidden="")
@@ -26,6 +31,25 @@ def index():
                 count_menu += 1
                 return render_template("index.html", hidden="hidden")
 
+
+@app.route("/<Username>/profile", methods=["GET", "POST"])
+def profile(Username):
+    global count_menu
+    if request.method == "GET":
+        count_menu = 0
+        return render_template("profile.html", name=Username, hidden="hidden",
+                               functional="Для вас доступен весь функционал сайта")
+    else:
+        if list(request.form.keys())[0] == "Sett":
+            if count_menu % 2 == 0:
+                count_menu += 1
+                return render_template("profile.html", hidden="", name=Username)
+            else:
+                count_menu += 1
+                return render_template("profile.html", hidden="hidden", name=Username)
+        elif list(request.form.keys())[0] == "Name":
+            return render_template("profile.html", name=Username, hidden="hidden",
+                               functional="Для вас доступен весь функционал сайта")
 
 @app.route('/reg', methods=["GET", "POST"])
 def reg():
@@ -35,13 +59,10 @@ def reg():
 
 @app.route('/signin', methods=["GET", "POST"])
 def signin():
-    if request.method == "GET":
-        return render_template("signin.html")
-
-
-@app.route("/<Username>/profile")
-def profile(Username):
-    return render_template("profile.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('signin.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
