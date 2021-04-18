7f5d5c9ca9d46b1b3500b1ecfdb85143e154bbe3
GovardFilipsovich <ricardrajonhart@gmail.com>
2021-04-18 16:59:19 +0300

    Продолжена работа над редактором. Начата работа над классами основных элементов блок-схем.

--- static/js/editor-script.js
+++
@@ -1,8 +0,0 @@
-var example = document.getElementById("Canvas"),
-ctx = example.getContext('2d');
-example.height = 750;
-example.width  = 650;
-Img = new Image();Img.src = '/static/img/function.png';
-Img.onload = function() {
-ctx.drawImage(Img,50, 50);
-}
